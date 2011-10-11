f8e5ab6d6a189165062a84f638f6ac28d0312609
Jeff Buchbinder <jbuchbinder@ravemobilesafety.com>
2011-10-11 16:57:27 -0400

    Migrate javadoc compilation to ant.

--- makeJavadoc.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Creating Javadoc for RTP text/t140 library ..
-mkdir javadoc
-javadoc -d .\javadoc -doctitle "RTP text/t140 library" -doctitle "RTP text/t140 library" -header "RTP text/t140 library" -breakiterator -classpath src -overview .\javadoc-overview.html se.omnitor.media.protocol.text.t140 se.omnitor.media.content.text.t140 se.omnitor.protocol.rtp.text
-@pause
-
-
